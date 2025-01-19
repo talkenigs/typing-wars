1899a9d46915b6c27e0196ed8a063d182819d2c9
talkenigs <talkengis@gmail.com>
2025-01-19 21:23:13 +0200

    co

--- apps/socket/src/main.ts
+++ apps/socket/src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:4200','http://34.154.156.201:4200'], // Frontend domain
+    origin: '*',
     methods: 'GET,POST,PUT,DELETE', // Allowed methods
     credentials: true, // Allow cookies/auth headers
   });
